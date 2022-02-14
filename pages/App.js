6f02a2d232d117182e9d35cc50ea815e416b4dcc
Shuvam Yadav <shuvamyadav@Shuvams-MacBook-Pro.local>
2022-02-14 11:43:04 +0530

    Added functionality to the code

--- pages/App.js
+++ pages/App.js
@@ -1,6 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 function MyApp({ Component, pageProps }) {
-	return <h1>Hello World</h1>
+	function addTodo(){
+		setTodo([...todo,input])
+		console.log(todo)
+		setInput("")
+	}
+	function deleteTodo(e){
+		setTodo((todo)=>(todo.filter((i)=>i!=e)))
+		console.log("deleted")
+	}
+	const [todo,setTodo] = useState([]);
+	const [input,setInput]= useState("");
+	return(
+	<>
+	<h1>Todo List</h1>
+	<h2>Add Todo</h2>
+	<input
+            type="text"
+            name="todo"
+			value={input}
+			onChange={(e)=>{
+				setInput(e.target.value)
+			}}
+    />
+	<button name="submit"type="submit" onClick={addTodo}>Add</button>
+	<div><h3>Todos</h3>
+		<ul>{todo.map((e)=><li>{e}<button name="delete" type="submit" onClick={()=>deleteTodo(e)}>Delete</button></li>)}</ul>
+	</div>
+	</>
+	);
 }
 
 export default MyApp
